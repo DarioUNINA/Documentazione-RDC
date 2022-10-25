8ed27cddb17ef34eadd37d68ac27f3b44e2e8a7f
gianmadd <gi.addati@studenti.unina.it>
2022-10-25 10:59:02 +0200

    Modified while true

--- BotNet/serverSocket.py
+++ BotNet/serverSocket.py
@@ -6,10 +6,13 @@ serverSocket.bind( ('',serverPort) ) #collega la socket al port 1200
                                     #la variabile passata e' una t-pla, indicata con la parentesi tonda e NON e' modificabile
 serverSocket.listen(1) #si mette in ascolto, 1 indica la dimensione della coda dei client
 print ('The server is ready to receive')
+
+
+connectionSocket, addr = serverSocket.accept()
+print('Accepted a new client', addr)
+
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    print('Accepted a new client', addr)
     sentence = connectionSocket.recv(1024).decode() #1024 e' il numero max di byte da ricevere, decode serve per rendere il codice indipendente dall' architettura
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    #connectionSocket.close()
